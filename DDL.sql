31e5679ad5eab6b3d39208ad2a02e333b5a0b8e8
davidsteinhafel <dasteinhafel@gmail.com>
2021-05-12 10:41:50 -0500

    saved and pushed incorrect file. Correct file being pushed.

--- DDL.sql
+++ DDL.sql
@@ -1,1 +1,22 @@
-SELECT * FROM taskcomplete.customers;
+SHOW databases;
+CREATE DATABASE taskComplete;
+CREATE TABLE customers (
+customer_id int NOT NULL auto_increment,
+name  varchar(100) NOT NULL,
+age int(11) NOT NULL,
+primary key (customer_id)
+);
+CREATE TABLE products (
+product_id int NOT NULL AUTO_increment,
+name varchar(100) not null,
+primary key (product_id)
+);
+
+CREATE TABLE orders (
+order_id int NOT NULL AUTO_INCREMENT,
+fk_customer_id int NOT NULL,
+fk_product_id int NOT NULL,
+PRIMARY KEY (order_id),
+FOREIGN KEY (fk_customer_id) REFERENCES customers(customer_id),
+FOREIGN KEY (fk_product_id) REFERENCES products(product_id)
+);
